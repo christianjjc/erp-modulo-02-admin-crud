e3ebb0add9d329bed6b49f8cc186a6a3a419c4e6
Christian Jiménez Calvo <christianjjc@gmail.com>
2024-02-19 10:55:04 -0500

    Se crea el cliente prisma y se genera todo en la base de datos
     On branch feature/primera-migracion
     Changes to be committed:
    	modified:   README.md
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   prisma/migrations/20240219153742_first/migration.sql
    	new file:   prisma/migrations/migration_lock.toml
    	modified:   prisma/schema.prisma
    	new file:   src/seed/seed-database.ts
    	new file:   src/seed/seed.ts
    	new file:   src/seed/tsconfig.json

---
+++ prisma/migrations/20240219153742_first/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('super', 'admin', 'user', 'client');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'user',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "email" TEXT NOT NULL,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
